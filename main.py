ff0d0543f8e36d6b301e7a77c023b7779b6a97fc
bringbackthedog <bringbackthedog@gmail.com>
2021-11-18 10:49:26 -0800

    +templated variables

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
+name="Lenz Paul"
+role="Software Engineer"
+phone="+1-778-268-1456"
+email="info@lenzpaul.dev"
+location="Vancouver, BC, Canada"
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", 
+    name=name,
+    role=role,
+    phone=phone,
+    email=email
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
